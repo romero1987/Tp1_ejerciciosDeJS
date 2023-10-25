64f8e632293bd2035919c94ae5c444cfe6b15b1c
Luis Romero <1987romeroluisalberto@gmail.com>
2023-10-25 16:54:20 -0300

    tp1 realizado el ejercicio 6

---
+++ js/ejercicio_07.js
@@ -0,0 +1,9 @@
+let numero1 = parseInt(prompt('ingrese el primer numero'));
+let numero2 = parseInt(prompt('ingrese el segundo numero'));
+let numero3 = parseInt(prompt('ingrese el segundo numero'));
+
+if(numero1 > numero2 && numero1 > numero3){
+    document.write(`El numero 1 (${numero1}) es mayor que el numero 2 (${numero2}) y 3 (${numero3})` );
+} else if(numero3 > numero2){
+    
+}
