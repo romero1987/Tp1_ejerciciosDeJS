809bcfa1e3c7f3a74b95a1e3a194972ec86fb376
Luis Romero <1987romeroluisalberto@gmail.com>
2023-10-26 15:52:08 -0300

    tp1 realizado el ejercicio 08

---
+++ js/ejercicio_09.js
@@ -0,0 +1,6 @@
+let numero = parseInt(prompt('ingrese un numero'));
+if(numero %2 == 0){
+    document.write(`El numero ${numero} es divisible por 2`)
+} else {
+    document.write(`El numero ${numero} NO es divisible por 2`)
+}
